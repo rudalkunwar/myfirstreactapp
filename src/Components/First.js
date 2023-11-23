099bc544967a413a35bb3a39d727c0701c33adb8
ruzal <ruzalkunwar@gmail.com>
2023-11-23 11:19:54 +0545

    firstcommit

---
+++ src/Components/First.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class First extends Component{
+render(){
+    return(
+
+        <div>
+        <h2>My name ankuha</h2>
+        </div>
+    );
+}
+}
+
+export default First;
