d72d6e4f534013f749d5696c8cc0a126eedd22f0
ruzal <ruzalkunwar@gmail.com>
2023-11-30 12:06:29 +0545

    map function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import './App.css';
-import Navbar from './Components/Navbar/navbar';
-import Number from './Components/Numbers';
-import Num from './Components/Num';
-import Namee from './Components/Nameprops';
-import Eventhandeler from './Components/Eventhandle';
-import Footer from './Components/Footer/Footer';
-function App() {
-  return (
-    <div>
-      <Navbar/>
-      {/* <Num num="432"/> */}
-      <Number/>
-      <Namee name="rudal kunwar xoxoxo"/>
-      <Eventhandeler/>
-      <Footer/>
-    </div>
-  );
+import "./App.css";
+import Navbar from "./Components/Navbar/navbar";
+import Number from "./Components/Numbers";
+import Num from "./Components/Num";
+import Namee from "./Components/Nameprops";
+import Eventhandeler from "./Components/Eventhandle";
+import Footer from "./Components/Footer/Footer";
+import axios from "axios";
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      fruits: [
+        {
+          name: "orange",
+          colour: "orange",
+        },
+        {
+          name: "banana",
+          colour: "yellow",
+        },
+        {
+          name: "Apple",
+          colour: "Red",
+        },
+        {
+          name: "Grapes",
+          colour: "blue",
+        },
+      ],
+    };
+  }
+  render() {
+    let fruits;
+    fruits = this.state.fruits.map((p) => {
+      return <Namee name={p.name} colour={p.colour} />;
+    });
+    return (
+      <div>
+        <Navbar />
+        <Number />
+
+        <Eventhandeler />
+        <Footer />
+        {fruits}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Nameprops.js
+++ src/Components/Nameprops.js
@@ -1,15 +1,32 @@
-import React,{Component} from "react";
-class Namee extends Component{
-    constructor(props){
-        super(props);
-        this.props=props;
-    }
-render(){
-    return(
-        <div>
-            Hi my name is {this.props.name}.
+import React, { Component } from "react";
+class Namee extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    const style = {
+      height: "200px",
+      display: "flex",
+      allignItem: "center",
+      justifyContent: "center",
+    };
+    const pstyle = {
+      color: "red",
+      boxShadow: "0px 0px 5px blue",
+      width:"50%"
+    };
+    return (
+      <div style={style}>
+        <div style={pstyle}>
+          <ul>
+            <li><span>Name:</span> <span> {this.props.name}</span></li>
+            <li><span>Colour:</span> <span> {this.props.colour}</span></li>
+          </ul>
         </div>
+      </div>
     );
-}
+  }
 }
 export default Namee;
