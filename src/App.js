843f36202d5d3d71150f5c26750f2ec6784bda43
ruzal <ruzalkunwar@gmail.com>
2023-12-20 20:53:18 +0545

    show hide pass

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       email: null,
       password: null,
       isRegistered: false,
+      isShow:false
     };
   }
   RegistrationHandler = (event) => {
@@ -20,11 +21,24 @@ class App extends Component {
     this.setState({ name, email, password, isRegistered: true });
     event.preventDefault();
   };
+  ShowHidepass = ()=>{
+
+    if(this.state.isShow){
+      this.setState({
+        isShow:false,
+      });
+    }else{
+      this.setState({
+        isShow:true,
+      });
+    }
+ 
+  }
   render() {
     return (
       <div className="App">
         {!this.state.isRegistered ? (
-          <Registration register={this.RegistrationHandler} />
+          <Registration register={this.RegistrationHandler} togglepass={this.ShowHidepass} status={this.state.isShow} />
         ) : (
           <Welcome name={this.state.name} email={this.state.email} />
         )}
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,8 +1,32 @@
 import React from "react";
 export default function Registration(props) {
+  let passType;
+  const iclass = [
+  "text-2xl",
+  "absolute",
+  "right-0",
+  "inset-y-0",
+  "items-center",
+  "flex",
+  "pr-2"];
+  if(props.status){
+      passType="text";
+      iclass.push("ri-eye-off-line");
+      console.log(iclass);
+  }
+  else{
+      passType="password";
+      iclass.push("ri-eye-line");
+      console.log(iclass);
+
+    }
   return (
+    
     <div>
-      <form onSubmit={props.register} className="h-screen bg-yellow-300 px-10 flex justify-center items-center">
+      <form
+        onSubmit={props.register}
+        className="h-screen bg-yellow-300 px-10 flex justify-center items-center"
+      >
         <div>
           <input
             className="block p-2 px-5 mb-2 rounded "
@@ -18,13 +42,19 @@ export default function Registration(props) {
             name="email"
             required
           ></input>
-          <input
-            className="block p-2 px-5 mb-2 rounded "
-            placeholder="Password"
-            type="password"
-            name="password"
-            required
-          ></input>
+          <div className="relative">
+            <input
+              className="block p-2 px-5 mb-2 rounded "
+              placeholder="Password"
+              type={passType}
+              name="password"
+              required
+            />{" "}
+            <i
+              onClick={props.togglepass}
+              className={iclass.join(" ")} 
+            ></i>
+          </div>
           <input
             className="block p-2 px-5 mb-2 rounded bg-green-500 w-full text-white hover:bg-green-400"
             type="submit"
@@ -32,6 +62,8 @@ export default function Registration(props) {
           ></input>
         </div>
       </form>
+
+      {/* <i class="ri-eye-off-line"></i> */}
     </div>
   );
 }
