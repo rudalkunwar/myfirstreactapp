9576ed05a0f15603edc4ea3cf9a582a15e93c19e
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-28 20:40:09 +0545

    useStatehook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 
 export default function App() {
   const [text,setText] = useState('');
-  const textFun =  () => {
-
+  const textFun =  (e) => {
+    setText(text=>e.target.value);
   }
   return (
-    <div>
-    <textarea onKeyUp={textFun} cols="30" rows="10"></textarea>
+    <div className='flex h-screen flex-col justify-center items-center bg-gray-400'>
+    <textarea className='bg-cyan-100 outline-none p-2 border border-yellow-500' onKeyUp={textFun} cols="100" rows="10"></textarea>
+    <textarea className='mt-5 bg-yellow-100 outline-none p-2 border'cols="100" rows="10" >dfsadfafda</textarea>
     </div>
   )
 }
