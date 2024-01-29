36c69c923c27dbc02c8e5d61f89c872fe7b6dfdb
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-29 18:49:24 +0545

    start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ export default function App() {
   return (
     <div className='flex h-screen flex-col justify-center items-center bg-gray-400'>
     <textarea className='bg-cyan-100 outline-none p-2 border border-yellow-500' onKeyUp={textFun} cols="100" rows="10"></textarea>
-    <textarea className='mt-5 bg-yellow-100 outline-none p-2 border'cols="100" rows="10" >dfsadfafda</textarea>
+    <textarea className='mt-5 bg-yellow-100 outline-none p-2 border'cols="100" rows="10" value={text} ></textarea>
+    <iframe src="https://www.hamropatro.com/widgets/dateconverter.php" frameborder="0" scrolling="no" marginwidth="0" marginheight="0"  allowtransparency="true"></iframe>
     </div>
   )
 }
