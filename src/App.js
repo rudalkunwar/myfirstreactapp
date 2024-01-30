bb7875ec3e2f59c51b85f3e07fc376177ae3ddbe
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-30 20:15:52 +0545

    restart

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { Component } from "react";
-import Auth from "./Components/practice/Auth";
-import { useState } from "react";
-export default function App() {
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const userAuth = (event) => {
-    event.preventDefault();
-    const formdata = new FormData(event.target);
-    const username = formdata.get("username");
-    const email = formdata.get("email");
-    const password = formdata.get("password");
+import React from 'react'
+import Home from './Components/react2/Home'
 
-    setUsername(username);
-    setEmail(email);
-    setPassword(password);
-  };
+export default function App() {
   return (
     <div>
-      <Auth authenticate={userAuth} />
-      <div>
-        <h1>details</h1>
-        <ul>
-        <li>{username}</li>
-          <li>{email}</li>
-          <li>{password}</li>
-        </ul>
-      </div>
+      <Home/>
     </div>
-  );
+  )
 }
---
+++ src/Components/react2/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+         <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
