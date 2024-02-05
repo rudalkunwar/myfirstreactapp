0839df76b4facd152b5dc24c9fab23ece5c8fe20
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-05 17:55:51 +0545

    redux practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Home from "./Home";
-import { Provider, useSelector } from "react-redux";
-import mystore from "./store/store";
-import { myredux } from "./Reducer";
+
 function App() {
-  //   const { count } = useSelector((state) => state.myredux);
   return (
     <div>
-      <Provider store={mystore}>
         <Home />
-      </Provider>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,9 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react'
+
 function Home() {
-  const dispatch = useDispatch();
-  const { count } = useSelector((state) => state.myredux);
-  const increment = () => {
-    dispatch({
-      type: "increment",
-    });
-  };
-  const decrement = () => {
-    dispatch({
-      type: "decrement",
-    });
-  };
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="text-3xl text-center">
-        <h1>{count}</h1>
-        <div className="py-5 flex justify-between">
-          <button
-            onClick={increment}
-            className="bg-green-600 px-5 py-3 rounded-full text-white mr-4"
-          >
-            +
-          </button>
-          <button
-            onClick={decrement}
-            className="bg-red-400 px-5 py-3 rounded-full text-white ml-4"
-          >
-            -
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
-export default Home;
+export default Home
