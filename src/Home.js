6833b0ad61ef7552af37d3db591e12e57fa49681
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-05 18:05:40 +0545

    home componenet

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-cyan-300 h-screen flex justify-center items-center'>
+      <div className='flex'>
+        <div><button className='px-5 py-2 bg-blue-400 rounded-full'>+</button></div>
+        <div className='px-5'>0</div>
+        <div><button className='bg-red-400 px-5 py-2 rounded-full'>-</button></div>
+      </div>
+    </div>
   )
 }
 
