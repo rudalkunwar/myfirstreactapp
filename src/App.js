002a68b4cd8551144e9c6dc83d1ebd5d9bf4d61c
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-05 21:31:10 +0545

    redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Home from "./Home";
-
+import { Provider } from "react-redux";
+import Store from "./store/Store";
 function App() {
   return (
     <div>
+      <Provider store={Store}>
         <Home />
+      </Provider>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { increment,decrement } from "./counterSlice";
 function Home() {
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className='bg-cyan-300 h-screen flex justify-center items-center'>
-      <div className='flex'>
-        <div><button className='px-5 py-2 bg-blue-400 rounded-full'>+</button></div>
-        <div className='px-5'>0</div>
-        <div><button className='bg-red-400 px-5 py-2 rounded-full'>-</button></div>
+    <div className="bg-cyan-300 h-screen flex justify-center items-center">
+      <div className="flex">
+        <div>
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-5 py-2 bg-blue-400 rounded-full"
+          >
+            +
+          </button>
+        </div>
+        <div className="px-5">0</div>
+        <div>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="bg-red-400 px-5 py-2 rounded-full"
+          >
+            -
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
