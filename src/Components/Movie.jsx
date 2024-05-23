2beab7d667503c0b193cc2e01d6a22463793061b
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-05-23 13:03:09 +0545

    hor bar

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,74 +1,23 @@
 import React from "react";
-const movies = [
-  {
-    movieId: 11,
-    name: "Inception",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    movieId: 12,
-    name: "The Dark Knight",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
-  },
-  {
-    movieId: 13,
-    name: "The Matrix",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    movieId: 14,
-    name: "Fight Club",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNDIzODRmZTEtMmNhNS00ZjEwLWI5YzQtMjZhZDY3ZDJkYmE0XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    movieId: 15,
-    name: "Pulp Fiction",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNGNhM5BlZTAtM2UwYTAtYWYwYy00ZDc2LWJmNWYtYzY5YzE3ZjEyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-  },
-  {
-    movieId: 16,
-    name: "The Shawshank Redemption",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmQtZWFjYy00YzBkLWJmYjQtOWRiZmQxMWVmYjI5XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-  },
-  {
-    movieId: 17,
-    name: "Gladiator",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzNhMWUzODY1MmEwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    movieId: 18,
-    name: "Saving Private Ryan",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDQtYWY5Yzg3ZGRkZjM4XkEyXkFqcGdeQXVyMDM2NDM2MA@@._V1_SX300.jpg",
-  },
-];
+import movies from "./movies";
 function Movie() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="bg-blue-300 h-52 w-48 flex gap-4">
-        <div className="relative">
-          <img
-            className="absolute"
-            src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-            alt=""
-          />
-          <p>Inception</p>
-        </div>
-        <div className="relative">
-          <img
-            className="absolute"
-            src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-            alt=""
-          />
-          <p>Inception</p>
-        </div>
+      <div className="relative flex items-center overflow-x-scroll">
+        {movies.map((movie) => (
+          <div key={movie.movieId} className="mx-6">
+            <div className="relative">
+              <img
+                className="h-52 w-[500px] object-cover"
+                src={movie.poster}
+                alt={`${movie.name} poster`}
+              />
+              <div className="absolute bottom-0 w-full bg-opacity-75 bg-red-500 text-white text-center">
+                <p>{movie.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/Components/movies.js
@@ -0,0 +1,72 @@
+const movies = [
+  {
+    movieId: 11,
+    name: "Inception",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    movieId: 12,
+    name: "The Dark Knight",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+  },
+  {
+    movieId: 13,
+    name: "The Matrix",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    movieId: 14,
+    name: "Fight Club",
+    poster: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_SY679_.jpg",
+  },
+  {
+    movieId: 15,
+    name: "Pulp Fiction",
+    poster: "https://m.media-amazon.com/images/I/71c05lTE03L._AC_SY679_.jpg",
+  },
+  {
+    movieId: 16,
+    name: "The Shawshank Redemption",
+    poster: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_SY679_.jpg",
+  },
+  {
+    movieId: 11,
+    name: "Inception",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    movieId: 12,
+    name: "The Dark Knight",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+  },
+  {
+    movieId: 13,
+    name: "The Matrix",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    movieId: 13,
+    name: "The Matrix",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    movieId: 11,
+    name: "Inception",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    movieId: 16,
+    name: "The Shawshank Redemption",
+    poster: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_SY679_.jpg",
+  },
+];
+
+export default movies;
